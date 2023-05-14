0163703a86b150e2547610e1bf29de22f058d197
mcho295 <michaelcho295@gmail.com>
2023-05-14 10:07:41 -0400

    add day7 part1

---
+++ 2022/day7/day7_iterative.py
@@ -0,0 +1,35 @@
+from collections import defaultdict
+
+# Read input data
+f = open("input7.txt", "r")
+data = f.read()
+
+# Part 1
+commands = data.split("\n")
+
+dir_stack = []
+size_map = defaultdict(int)
+
+for command in commands:
+    comp = command.split()
+    if comp[0] == "$":
+        if comp[1] == "cd":
+            if comp[2] != "..":
+                if len(dir_stack) > 0:
+                    dir_stack.append(dir_stack[-1] + "\\" + comp[2])
+                else:
+                    dir_stack.append(comp[2])
+            else:
+                dir_stack.pop()
+    else:
+        if comp[0] != "dir":
+            for dir in dir_stack:
+                size_map[dir] += int(comp[0])
+
+answer = 0
+
+for key, value in size_map.items():
+    if value < 100000:
+        answer += value
+
+print(answer)
